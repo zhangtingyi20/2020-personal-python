5d58808539dc55660cbb8d717d75c748d4c7b30f
张廷宜 <70851325+zhangtingyi20@users.noreply.github.com>
2020-09-16 18:50:40 +0800

    Update GHAnalysis.py

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -51,8 +51,8 @@ def findnum(name,repo,event):
 					date = json.loads(jsonstr)
 					if(date['actor']['login']==name and date['repo']['name']==repo):
 						num=num+1
-	elif(repo==' ' and name ==' '):#初始化输出0
-		#return
+	#初始化输出0
+	
 	print(num)
 
 		
